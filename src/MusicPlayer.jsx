354ca400d31860901ce59d72f2e73e3e4271fdb4
Jaymiller2002 <jay.miller02@icloud.com>
2024-08-10 12:06:46 -0400

    I think this works

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -2,6 +2,28 @@ import React, { useState, useRef, useEffect } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 import './MusicPlayer.css';
 
+import track1Image from './assets/images/beach-5526592_1280.jpg';
+import track2Image from './assets/images/milky-way-2695569_1280.jpg';
+import track3Image from './assets/images/milky-way-5673357_1280.jpg';
+import track4Image from './assets/images/night-4695490_1280.jpg';
+import track5Image from './assets/images/night-sky-6039591_1280.jpg';
+import track6Image from './assets/images/plouzane-1758197_1280.jpg';
+import track7Image from './assets/images/road-4055838_1280.jpg';
+import track8Image from './assets/images/seascape-4636264_1280.jpg';
+import track9Image from './assets/images/sunset-5136129_1280.jpg';
+import track10Image from './assets/images/thunderstorm-3625405_1280.jpg';
+
+import track1Mp3 from './assets/mp3/summer-rain-lofi-vibes-216043.mp3';
+import track2Mp3 from './assets/mp3/good-night-160166.mp3';
+import track3Mp3 from './assets/mp3/for-a-dream-lofi-vibes-216038.mp3';
+import track4Mp3 from './assets/mp3/fat-chillin-196099.mp3';
+import track5Mp3 from './assets/mp3/workout-trap-172029.mp3';
+import track6Mp3 from './assets/mp3/good-morning-144685.mp3';
+import track7Mp3 from './assets/mp3/playa-del-sol-latin-lofi-160149.mp3';
+import track8Mp3 from './assets/mp3/virtual-insanity-208607.mp3';
+import track9Mp3 from './assets/mp3/Too Close To Touch & Bad Omens - _Novocaine_.mp3';
+import track10Mp3 from './assets/mp3/Cleffy -  Meet you at the Graveyard (Official Lyric Video).mp3';
+
 const MusicPlayer = () => {
   const [currentTrack, setCurrentTrack] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
@@ -10,55 +32,54 @@ const MusicPlayer = () => {
   const tracks = [
     {
       name: 'Track 1',
-      url: '/assets/mp3/summer-rain-lofi-vibes-216043.mp3',
-      image: '/assets/images/beach-5526592_1280.jpg'
+      url: track1Mp3,
+      image: track1Image
     },
     {
       name: 'Track 2',
-      url: '/assets/mp3/good-night-160166.mp3',
-      image: '/assets/images/milky-way-2695569_1280.jpg'
+      url: track2Mp3,
+      image: track2Image
     },
     {
       name: 'Track 3',
-      url: '/assets/mp3/for-a-dream-lofi-vibes-216038.mp3',
-      image: '/assets/images/milky-way-5673357_1280.jpg'
+      url: track3Mp3,
+      image: track3Image
     },
     {
       name: 'Track 4',
-      url: '/assets/mp3/fat-chillin-196099.mp3',
-      image: '/assets/images/night-4695490_1280.jpg'
+      url: track4Mp3,
+      image: track4Image
     },
     {
       name: 'Track 5',
-      url: '/assets/mp3/workout-trap-172029.mp3',
-      image: '/assets/images/night-sky-6039591_1280.jpg'
+      url: track5Mp3,
+      image: track5Image
     },
     {
       name: 'Track 6',
-      url: '/assets/mp3/good-morning-144685.mp3',
-      image: '/assets/images/plouzane-1758197_1280.jpg'
+      url: track6Mp3,
+      image: track6Image
     },
     {
       name: 'Track 7',
-      url: '/assets/mp3/playa-del-sol-latin-lofi-160149.mp3',
-      image: '/assets/images/road-4055838_1280.jpg'
+      url: track7Mp3,
+      image: track7Image
     },
     {
       name: 'Track 8',
-      url: '/assets/mp3/virtual-insanity-208607.mp3',
-      image: '/assets/images/seascape-4636264_1280.jpg'
+      url: track8Mp3,
+      image: track8Image
     },
     {
       name: 'Track 9',
-      url: '/assets/mp3/Too Close To Touch & Bad Omens - _Novocaine_.mp3',
-      image: '/assets/images/sunset-5136129_1280.jpg'
+      url: track9Mp3,
+      image: track9Image
     },
     {
       name: 'Track 10',
-      url: '/assets/mp3/Cleffy -  Meet you at the Graveyard (Official Lyric Video).mp3',
-      image: '/assets/images/thunderstorm-3625405_1280.jpg'
+      url: track10Mp3,
+      image: track10Image
     },
-    // Add more tracks here
   ];
 
   const playPause = () => {
